2772ad4db9616f7f559c1952513c8781f6249105
Gaurab Das <gaurab.working@gmail.com>
2024-04-03 21:01:25 +0530

    Add files via upload

---
+++ Backend/index.js
@@ -0,0 +1,26 @@
+const  express= require('express');
+const blogsRoutes= require('./routes/companyRoutes');
+const app=express();
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+
+app.listen(7000, () => {
+    console.log('Listening on port 7000');
+  });
+ app.use('/',companyRoutes);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
